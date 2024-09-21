b48729928e63750764709c4d4ebeb96732952ece
MinQan141104 <minhquan141104@gmail.com>
2024-09-21 23:44:23 +0700

    no message

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () =>{
     const { id } = useParams()
+    const { data, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            { isPending && <div>Loading...</div>}
+            { error && <div>{ error }</div>}
+            { data && (
+                <article>
+                    <h2> {data.title} </h2>
+                    <p>Written by { data.author }</p>
+                    <div>{ data.body }</div>
+                </article>
+            )}
         </div>
     );
 }
