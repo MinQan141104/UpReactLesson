385ba8cb1af146d03f9df9fd52afbbc1c521fc26
MinQan141104 <minhquan141104@gmail.com>
2024-09-22 00:11:49 +0700

    no message

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
-    return (
-      <div className="create">
-        <h2>Add a New Blog</h2>
-      </div>
-    );
-  }
-   
-  export default Create;
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title: </label>
+        <input type="text"
+         required 
+         value={title}
+         onChange={(e) => setTitle(e.target.value)}/>
+        <label>Blog Body: </label>
+        <textarea required
+        value={body}
+        onChange={(e) => setBody(e.target.value)}>
+        </textarea>
+        <label>Blog Author: </label>
+        <select value={ author }
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Quan">Quan</option>
+          <option value="Cuong">Cuong</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+}
+
+export default Create;
