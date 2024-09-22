a7a4b83e183599e23238c9eb2d66662672b69ff0
MinQan141104 <minhquan141104@gmail.com>
2024-09-22 18:44:01 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path='*' element={<NotFound/>}/>
+            //Nếu để path='*' ở cuối tức là ngoài các path dc nêu trên thì ngoại lệ sẽ
+            // rơi vào đây. nếu để ở đầu nó sẽ nhận tất cả đường dẫn bất kì.
           </Routes>
         </div>
       </div>
